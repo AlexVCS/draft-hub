fcd95912718c42f7e6209d7c1f36824bf9a2ce97
alexvcs <alexcurtisslep@gmail.com>
2025-05-27 15:40:37 -0400

    clean up profile page, removed unused imports and logic

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,31 +1,11 @@
-import Avatar from "@mui/material/Avatar";
-import Button from "@mui/material/Button";
-import {
-  bioWithRanks,
-  calculateAge,
-  convertToFeetInchesAndMeters,
-  convertToKilos,
-  seasonStats,
-} from "../helpers/helper_funcs";
-import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
-import CardContent from "@mui/material/CardContent";
-import Typography from "@mui/material/Typography";
-import {useParams} from "react-router";
 import {Box} from "@mui/material";
 import PlayerCard from "./PlayerCard";
 
 const Profile = () => {
-  const player = useParams();
-
-  const findPlayerBio = bioWithRanks.filter(
-    (p) => player.playerId == p.playerId
-  );
   // const findPlayerSeasonStats = seasonStats.filter(
   //   (p) => player.playerId == p.playerId
   // );
 
-  console.log(findPlayerBio);
 
   return (
     <Box>
