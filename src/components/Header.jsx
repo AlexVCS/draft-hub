ba73ccb112c7030423f8fa18002bc48fe31b8ebc
alexvcs <alexcurtisslep@gmail.com>
2025-06-06 12:01:42 -0400

    remove Mavs logo from the project. closes #32

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,8 @@ import Typography from "@mui/material/Typography";
 
 const Header = () => {
   return (
-    <Box sx={{display: "flex", flexDirection: "column", alignItems: "center", paddingBottom: '10px'}}>
-      <img width={200} height={200} src="/mavs_logo.png" alt="Mavs logo" />
+    <Box sx={{display: "flex", flexDirection: "column", alignItems: "center", padding: '10px'}}>
+      <img width={96} src="/nba_logo.svg" alt="NBA logo" />
       <Typography variant="h5">2025 Draft Hub</Typography>
     </Box>
   );
